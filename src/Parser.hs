7e93b1ff16a67210ebea13affb843d2c6322c90c
Balint  Rikker <balint.rikker14@imperial.ac.uk>
2016-01-09 18:05:20 +0100

    Parse programs

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,22 +1,46 @@
 module Parser where
 import Types
 
-import Text.Parsec as P
+import Text.Parsec 
+import Data.Map (fromList)
 
-parseIncInstruction :: P.Parsec String () Instruction
-parseIncInstruction = do
-    P.char 'R'
-    regNum <- P.many1 P.digit
-    iType <- P.char '+'
-    P.spaces
-    P.string "->"
-    P.spaces
-    label <- parseLabel
-    return (Inc (Reg $ read regNum) label)
+program :: Parsec String () Program
+program = do
+    spaces
+    prog <- many $ (,) 
+                <$> instLabel <*> (haltInstruction <|> regInstruction)
+    return $ fromList prog
 
-parseLabel :: P.Parsec String () Label
-parseLabel = do
-    P.char 'L'
-    num <- P.many1 P.digit
+regInstruction :: Parsec String () Instruction
+regInstruction = do
+    r <- register
+    t <- oneOf "+-"
+    spaces
+    string "->"
+    spaces
+    case t of
+        '+' -> do
+            l <- instLabel
+            return (Inc r l)
+        '-' -> do
+            l1 <- instLabel
+            spaces
+            char ','
+            spaces
+            l2 <- instLabel
+            return (Dec r l1 l2)
+
+haltInstruction :: Parsec String () Instruction
+haltInstruction = (\_ -> Halt) <$> string "HALT"
+
+instLabel :: Parsec String () Label
+instLabel = do
+    char 'L'
+    num <- many1 digit
     return (Lab $ read num)
     
+register :: Parsec String () Register
+register = do
+    char 'R'
+    n <- many1 digit
+    return (Reg $ read n)
